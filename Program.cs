5e971ba0b0421ef2a9a9210174ef4f8bf2c68d18
Breno Robert <64442888+BrenoRobertDiogo@users.noreply.github.com>
2021-06-10 10:56:07 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using tabuleiro;
+using xadrez;
+namespace XadrezProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+                while (!partida.terminada)
+                {
+                    try
+                    {
+                        Console.Clear();
+                        
+                        Tela.imprimirPartida(partida);
+                        Console.Write("De: ");
+                        Posicao origem = Tela.posicaoDoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
+
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                        Console.Write("Para: ");
+                        Posicao destino = Tela.posicaoDoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
+
+                        partida.realizaJogada(origem, destino);
+
+                    }
+                    catch (TabuleiroException e)
+                    {
+
+                        Console.WriteLine(e.Message);
+                        Console.ReadKey();
+                    }
+
+
+                }
+            }
+            catch (TabuleiroException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Final do programa");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tela.cs
@@ -0,0 +1,119 @@
+﻿using tabuleiro;
+using System;
+using xadrez;
+using System.Collections.Generic;
+namespace XadrezProjeto
+{
+    class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                Console.Write(8 - i + "    ");
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    Tela.imprimirPeca(tab.peca(i, j));
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("     a   b   c   d   e   f   g   h");
+        }
+
+        public static void imprimirPartida(PartidaDeXadrez partida)
+        {
+            Tela.imprimirTabuleiro(partida.tab);
+            imprimirPecasCapturadas(partida);
+
+            Console.WriteLine(partida.turno + " turno");
+            Console.WriteLine("Aguardando " + partida.jogadorAtual + "s jogarem");
+            if (partida.xeque)
+            {
+            Console.WriteLine("XEQUE");
+
+            }
+
+            Console.WriteLine("\n===== MOVIMENTANDO PEÇA =====");
+        }
+
+        public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
+        {
+            Console.WriteLine("Peças capturadas: ");
+            Console.Write("Brancas: ");
+            imprimirConjunto(partida.pecasCapturadas(Cor.Branca));
+            Console.Write(" | Pretas: ");
+            imprimirConjunto(partida.pecasCapturadas(Cor.Preta));
+        }
+
+        public static void imprimirConjunto(HashSet<Peca> conjunto)
+        {
+            Console.Write("[");
+            foreach (Peca x in conjunto)
+            {
+                Console.Write(x+" ");
+            }
+            Console.Write("]");
+
+        }
+
+        public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoesPossiveis)
+        {
+            ConsoleColor fundoOriginal = Console.BackgroundColor;
+            ConsoleColor fundoAlterado = ConsoleColor.DarkGray;
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                Console.Write(8 - i + "    ");
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (posicoesPossiveis[i, j])
+                    {
+                        Console.BackgroundColor = fundoAlterado;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = fundoOriginal;
+                    }
+                    Tela.imprimirPeca(tab.peca(i, j));
+                    Console.BackgroundColor = fundoOriginal;
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("     a   b   c   d   e   f   g   h");
+            Console.BackgroundColor = fundoOriginal;
+        }
+
+        public static PosicaoXadrez posicaoDoXadrez()
+        {
+            string s = Console.ReadLine();
+            char linha = s[0];
+            int coluna = int.Parse(s[1] + "");
+            return new PosicaoXadrez(linha, coluna);
+        }
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca == null)
+            {
+                Console.Write("-   ");
+            }
+            else
+            {
+
+
+                ConsoleColor aux = Console.ForegroundColor;
+                if (peca.cor == Cor.Branca)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+                Console.Write("   ");
+            }
+        }
+    }
+}
